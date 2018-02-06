d3ba53bad01f50fc77d1e59b88b68bb728a3dc10
Cory Gillenkirk <cgillenkirk@gmail.com>
2018-02-06 04:41:10 -0500

    Fix formatting

--- commands/player.js
+++ commands/player.js
@@ -1,51 +1,49 @@
 			if (commandArgs.length > 0) {
-				let hypixelPlayer
+			    let hypixelPlayer
 
-				message.channel.startTyping()
+			    message.channel.startTyping()
 
-				try {
-					hypixelPlayer = (await HypixelClient.getPlayer('name', commandArgs[0])).player
-				}
-				catch (err) {
-					console.log(err)
-					message.channel.stopTyping()
-					message.channel.send('Hmm, that player doesn\'t seem to exist!')
-					return
-				}
+			    try {
+			        hypixelPlayer = (await HypixelClient.getPlayer('name', commandArgs[0])).player
+			    } catch (err) {
+			        console.log(err)
+			        message.channel.stopTyping()
+			        message.channel.send('Hmm, that player doesn\'t seem to exist!')
+			        return
+			    }
 
-				let playerRich = new Discord.RichEmbed()
+			    let playerRich = new Discord.RichEmbed()
 
-				playerRich.setThumbnail('https://crafatar.com/avatars/' + (hypixelPlayer.uuid || '') + '?size=100')
-				playerRich.setTitle('Hypixel Player: ' + hypixelPlayer.displayname)
-				playerRich.setURL('https://hypixel.net/player/' + hypixelPlayer.displayname + '/')
-				playerRich.setFooter('Hycord Bot | Created by ethanent', 'https://i.imgur.com/hFbNBr5.jpg')
-				playerRich.setColor('#30DB09')
+			    playerRich.setThumbnail('https://crafatar.com/avatars/' + (hypixelPlayer.uuid || '') + '?size=100')
+			    playerRich.setTitle('Hypixel Player: ' + hypixelPlayer.displayname)
+			    playerRich.setURL('https://hypixel.net/player/' + hypixelPlayer.displayname + '/')
+			    playerRich.setFooter('Hycord Bot | Created by ethanent', 'https://i.imgur.com/hFbNBr5.jpg')
+			    playerRich.setColor('#30DB09')
 
-                if (hypixelPlayer.newPackageRank) {
-					HypixelRank = hypixelPlayer.monthlyPackageRank === "SUPERSTAR" ? "MVP++" : hypixelPlayer.newPackageRank;
-                }
-				
-				playerRich.addField('Rank', (hypixelPlayer.rank || hypixelPlayer.packageRank || HypixelRank || 'None').toString().replace(/_/g, ' '), true)
-				playerRich.addField('Hypixel Level', hypixelPlayer.networkLevel || 'Not available', true)
-				playerRich.addField('Karma', hypixelPlayer.karma || 'Not available', true)
-				playerRich.addField('Client Version', hypixelPlayer.mcVersionRp || 'Not available', true)
-				playerRich.addField('First Login', hypixelPlayer.firstLogin ? moment(hypixelPlayer.firstLogin).calendar() : 'Not available', true)
-				playerRich.addField('Last Login', hypixelPlayer.lastLogin ? moment(hypixelPlayer.lastLogin).calendar() : 'Not available', true)
+			    if (hypixelPlayer.newPackageRank) {
+			        HypixelRank = hypixelPlayer.monthlyPackageRank === "SUPERSTAR" ? "MVP++" : hypixelPlayer.newPackageRank;
+			    }
 
-				let playerGuild
+			    playerRich.addField('Rank', (hypixelPlayer.rank || hypixelPlayer.packageRank || HypixelRank || 'None').toString().replace(/_/g, ' '), true)
+			    playerRich.addField('Hypixel Level', hypixelPlayer.networkLevel || 'Not available', true)
+			    playerRich.addField('Karma', hypixelPlayer.karma || 'Not available', true)
+			    playerRich.addField('Client Version', hypixelPlayer.mcVersionRp || 'Not available', true)
+			    playerRich.addField('First Login', hypixelPlayer.firstLogin ? moment(hypixelPlayer.firstLogin).calendar() : 'Not available', true)
+			    playerRich.addField('Last Login', hypixelPlayer.lastLogin ? moment(hypixelPlayer.lastLogin).calendar() : 'Not available', true)
 
-				let playerGuildID = (await HypixelClient.findGuild('member', hypixelPlayer.uuid)).guild
+			    let playerGuild
 
-				if (playerGuildID) {
-					playerGuild = (await HypixelClient.getGuild(playerGuildID)).guild
-				}
+			    let playerGuildID = (await HypixelClient.findGuild('member', hypixelPlayer.uuid)).guild
 
-				playerRich.addField('Guild', (playerGuild ? '[' + playerGuild.name + ' [' + playerGuild.tag + ']' + '](https://hypixel.net/guilds/' + playerGuild._id + '/)' : 'None'))
+			    if (playerGuildID) {
+			        playerGuild = (await HypixelClient.getGuild(playerGuildID)).guild
+			    }
 
-				message.channel.stopTyping()
+			    playerRich.addField('Guild', (playerGuild ? '[' + playerGuild.name + ' [' + playerGuild.tag + ']' + '](https://hypixel.net/guilds/' + playerGuild._id + '/)' : 'None'))
 
-				message.channel.send(playerRich)
-			}
-			else {
-				message.channel.send('Usage: `!player <name>`')
+			    message.channel.stopTyping()
+
+			    message.channel.send(playerRich)
+			} else {
+			    message.channel.send('Usage: `!player <name>`')
 			}
--- index.js
+++ index.js
@@ -5,64 +5,64 @@ const moment = require('moment')
 const args = process.argv.slice(2)
 
 if (args.length < 2) {
-	console.log('Usage: node index.js <Discord bot token> <Hypixel API key>')
-	process.exit(0)
+    console.log('Usage: node index.js <Discord bot token> <Hypixel API key>')
+    process.exit(0)
 }
 
 const client = new Discord.Client()
 const HypixelClient = new HypixelAPI(args[1])
 
 client.on('ready', () => {
-	client.user.setStatus('online')
-	client.user.setGame('!hycord')
+    client.user.setStatus('online')
+    client.user.setGame('!hycord')
 
-	console.log('The bot has been initialized!')
+    console.log('The bot has been initialized!')
 
-	let installedGuilds = client.guilds.array()
+    let installedGuilds = client.guilds.array()
 
-	console.log('This bot is available on ' + installedGuilds.length + ' guilds:')
+    console.log('This bot is available on ' + installedGuilds.length + ' guilds:')
 
-	let totalMembers = 0
+    let totalMembers = 0
 
-	for (let i = 0; i < installedGuilds.length; i++) {
-		totalMembers += installedGuilds[i].memberCount
-		console.log(installedGuilds[i].name + ': ' + installedGuilds[i].memberCount + ' members')
-	}
+    for (let i = 0; i < installedGuilds.length; i++) {
+        totalMembers += installedGuilds[i].memberCount
+        console.log(installedGuilds[i].name + ': ' + installedGuilds[i].memberCount + ' members')
+    }
 
-	console.log('Total members: ' + totalMembers)
+    console.log('Total members: ' + totalMembers)
 })
 
 client.on('message', async (message) => {
-	if (message.author.id === client.user.id) return
+    if (message.author.id === client.user.id) return
 
-	if (!message.guild || !message.member) {
-		if (message.channel.recipient) {
-			message.channel.send('To talk to me, get my attention in servers using the `!hycord` command!')
-		}
-		return
-	}
+    if (!message.guild || !message.member) {
+        if (message.channel.recipient) {
+            message.channel.send('To talk to me, get my attention in servers using the `!hycord` command!')
+        }
+        return
+    }
 
-	const messageContent = message.content
+    const messageContent = message.content
 
-	if (messageContent.indexOf('!') !== 0) {
-		return
-	}
+    if (messageContent.indexOf('!') !== 0) {
+        return
+    }
 
-	const commandComponents = messageContent.split('!')[1].split(' ')
-	const baseCommand = commandComponents[0].toLowerCase()
-	const commandArgs = (commandComponents.length > 1 ? commandComponents.slice(1) : [])
+    const commandComponents = messageContent.split('!')[1].split(' ')
+    const baseCommand = commandComponents[0].toLowerCase()
+    const commandArgs = (commandComponents.length > 1 ? commandComponents.slice(1) : [])
 
-	switch (baseCommand) {
-		case 'hycord':
-			require('../commands/hycord.js');
-			break
-		case 'player':
-			require('../commands/player.js');
-			break
-		case 'guild':
-			require('../commands/guild.js');
-			break
-	}
+    switch (baseCommand) {
+        case 'hycord':
+            require('../commands/hycord.js');
+            break
+        case 'player':
+            require('../commands/player.js');
+            break
+        case 'guild':
+            require('../commands/guild.js');
+            break
+    }
 })
 
 client.login(args[0])
